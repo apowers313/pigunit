b52510d600dfbdec7d51bc7f3caa2c74a9faceed
Adam Powers <apowers@ato.ms>
2013-11-10 21:01:50 -0800

    Converted to handlebars script and added node_modules path

--- support/pigunit-template.js
+++ support/pigunit-template.js
@@ -1,5 +1,7 @@
 // set java max heap size to 6GB. the default size (typically 128MB) dies on even the smallest test
 /* TODO: don't overwrite previous _JAVA_OPTIONS */
+require.paths = "{{moduleDir}}/node_modules";
+
 process.env['_JAVA_OPTIONS'] = "-Xmx6144m";
 var pigHome = process.env.PIG_HOME;
 var assert = require('assert');
@@ -18,27 +20,15 @@ java.classpath.push(pigJar);
 java.classpath.push(pigunitJar);
 
 // setup arguments for PigTest
-var pigScript = "top_queries.pig";
-var argsArray = [
-    "n=3"
-];
-var inputAlias = "data";
-var inputArray = [
-    "yahoo",
-    "yahoo",
-    "yahoo",
-    "twitter",
-    "facebook",
-    "facebook",
-    "linkedin"
-];
-var outputAlias = "queries_limit";
-var outputArray = [
-    "(yahoo,3)",
-    "(facebook,2)"
-];
-var testName = "should pass";
-var timeoutOption = 60000;
+/* XXX: these intermediate variables are nice for debugging, but they can go away when validation is implemented */
+var pigScript = "{{script}}";
+var argsArray = [{{#strArray args}}{{/strArray}}];
+var inputAlias = "{{inputAlias}}";
+var inputArray = [{{#strArray input}}{{/strArray}}];
+var outputAlias = "{{outputAlias}}";
+var outputArray = [{{#strArray output}}{{/strArray}}];
+var testName = "{{testDesc}}";
+var timeoutOption = {{options.timeout}};
 
 // convert args to Java arrays
 var pigArgs = java.newArray("java.lang.String", argsArray);
